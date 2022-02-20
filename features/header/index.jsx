1f4f2b375de847a48886b3ca105036d42701c61c
Denis P <denispochinskiy@yandex.ru>
2022-02-20 10:59:45 +0500

    added new pages

--- features/header/index.jsx
+++ features/header/index.jsx
@@ -1,23 +1,38 @@
-import React from 'react'
-import { Flex } from '@chakra-ui/react'
-import Link from 'next/link'
+import React from "react"
+import { Flex, Box } from "@chakra-ui/react"
+import Link from "next/link"
+import { Button } from "@chakra-ui/react"
+import Image from "next/image"
+import logo from "../img/logo.png"
 
 export default function Header() {
-	return (
-		<header>
-			<Flex  justifyContent="space-between">
-				<Flex>
-					<Link href="/">
-					Лого
-					</Link>
-				</Flex>
-				<Flex>
-					<Link href="/price">
-						Цены
-					</Link>
-				</Flex>
-				<Flex>3</Flex>
-			</Flex>
-		</header>
-	)
+  return (
+    <Box as="header" m={10} fontWeight="600">
+      <Flex justifyContent="space-between">
+        <Flex>
+          <Link href="/">
+            <Image
+              width="100"
+              height="100"
+              src={logo}
+              boxSize="100px"
+              objectFit="cover"
+            >
+            </Image>
+          </Link>
+        </Flex>
+        <Flex>
+          <Link href="/companies">Юридическим лицам</Link>
+          <Link href="/individuals">Физическим лицам</Link>
+          <Link href="/bankruptcy">Банкротство</Link>
+          <Link href="/map">Карта</Link>
+          <Link href="/price">Стоимость услуг</Link>
+        </Flex>
+        <Flex>
+          +79990123123
+          <Button>Заказать звонок</Button>
+        </Flex>
+      </Flex>
+    </Box>
+  )
 }
--- pages/map/Map.jsx
+++ pages/map/Map.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import { Box } from '@chakra-ui/react'
 
-export default function Price() {
+export default function Map() {
 	return (
 		<Box>
-  Стоимость услуг
+ Карта
 	</Box>
 	)
 }
