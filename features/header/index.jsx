f1086e816ac9849869640e1c24d4313e39839d55
Denis P <denispochinskiy@yandex.ru>
2022-02-22 01:05:44 +0500

    edit header/buttons, margins

--- features/header/index.jsx
+++ features/header/index.jsx
@@ -7,10 +7,10 @@ import logo from "../img/logo.png"
 
 export default function Header() {
   return (
-    <Box as="header" m={10} fontWeight="600">
+    <Box as="header" m={10} fontWeight="500">
       <Flex alignItems="center" justifyContent="space-between" m="-40px -30px">
         <Flex>
-          <Link href="/">
+          <Link href="/" cursor="pointer">
             <Image
               width="100"
               height="100"
@@ -20,18 +20,26 @@ export default function Header() {
             ></Image>
           </Link>
         </Flex>
-        <Flex justifyContent="space-between" flex="1 1 auto" m="0 30px">
+        <Flex justifyContent="space-between" flex="1 1 auto" marginX="6px" marginRight="50px">
           <Link href="/companies">Юридическим лицам</Link>
           <Link href="/individuals">Физическим лицам</Link>
           <Link href="/bankruptcy">Банкротство</Link>
           <Link href="/map">Карта</Link>
           <Link href="/price">Стоимость услуг</Link>
         </Flex>
-        <Flex alignItems="center" justifyContent="space-between">
+        <Flex
+          flexDirection="column"
+          alignItems="center"
+          justifyContent="space-between"
+        >
+          <Flex mt="15px">
+            <a href="tel:+78142332211">+7(814)233-22-11</a>
+          </Flex>
           <Flex>
-          +79990123123
-            </Flex>
-          <Flex><Button>Заказать звонок</Button></Flex>
+            <Button mt="10px" colorScheme="green" size="sm">
+              Заказать звонок
+            </Button>
+          </Flex>
         </Flex>
       </Flex>
     </Box>
