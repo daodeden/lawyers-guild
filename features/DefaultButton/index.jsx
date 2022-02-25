178c7891c13f570d58e555aadfa83a0ec4a3d8ba
sovofeel <sovofeel@gmail.com>
2022-02-25 21:49:40 +0500

    fix header

---
+++ features/DefaultButton/index.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Button } from "@chakra-ui/react"
+import { useMediaQuery } from "@chakra-ui/react"
+import colors from '../theme/colors'
+
+
+export default function DefaultButton({children, ...restProps}) {
+	const [isLargerThan1024] = useMediaQuery("(min-width: 1024px)")
+
+
+	const buttonProps = isLargerThan1024 ? {
+		fontSize: '14px'
+	} : {
+		fontSize: '12px'
+	}
+
+	
+
+	return (
+		<Button bgColor={colors.mainColor} colorScheme="cyan" color="#fff" {...buttonProps} {...restProps} >{children}</Button>
+	)
+}
--- features/header/index.jsx
+++ features/header/index.jsx
@@ -1,49 +1,42 @@
-import React from "react"
-import { Flex, Box, IconButton } from "@chakra-ui/react"
-import { Button } from "@chakra-ui/react"
-import Image from "next/image"
-import logo from "../img/logo.png"
-import { useState } from "react"
-import styles from "./header.module.css"
-import {
-  Icon,
-  HamburgerIcon,
-  AddIcon,
-  ExternalLinkIcon,
-  RepeatIcon,
-  EditIcon,
-} from "@chakra-ui/icons"
-import { Link } from "@chakra-ui/react"
-import { Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react"
-import { useMediaQuery } from "@chakra-ui/react"
+import React, { useEffect } from "react"
+import { Flex } from "@chakra-ui/react"
+import styles from '../header/header.module.css'
+import { Link, Text } from "@chakra-ui/react"
 import HamburgerMenu from "./HamburgerMenu/HamburgerMenu"
 import HeaderLinks from "./HeaderLinks/HeaderLinks"
 import PhoneNumber from "./Phone/Phone"
+import DefaultButton from '../DefaultButton'
 
 export default function Header() {
-  const [isLargerThan1024] = useMediaQuery("(min-width: 1024px)")
-  const [isLargerThan1024PhoneNumber] = useMediaQuery("(min-width: 1024px)")
-
+  useEffect(()  =>{
+    console.log(styles);
+  } , [])
+  
   return (
-    <Box as="header" m={10} fontWeight="500">
-      <Flex alignItems="center" justifyContent="space-between" m="-40px -30px">
-        <Flex>
+    <Flex as="header" mt="24px" mb="32px" justifyContent="center" fontWeight="500">
+      <Flex alignItems="center" justifyContent="space-between" px="30"  w="1300px">
+        <Flex >
           <Link href="/" cursor="pointer">
-            <Image
-              width="100"
-              height="100"
-              src={logo}
-              boxSize="100px"
-              objectFit="cover"
-            ></Image>
+            <Text>ГЮ</Text>
           </Link>
         </Flex>
-        {isLargerThan1024 ? <HeaderLinks /> : " "}
-        
-        {isLargerThan1024PhoneNumber ? <PhoneNumber />  : <HamburgerMenu /> }
-        
-        
+        <Flex className={styles.headerLinks}>
+        <HeaderLinks  /> 
+        </Flex>
+      <Flex className={styles.phoneNumber}>
+        <PhoneNumber />
+      </Flex> 
+      
+      
+      <Flex className={styles.getCall}>
+        <DefaultButton>
+          Заказать звонок
+        </DefaultButton>
       </Flex>
-    </Box>
+      <Flex className={styles.hamburgerMenu} >
+        <HamburgerMenu  /> 
+        </Flex>
+      </Flex>
+    </Flex>
   )
 }
